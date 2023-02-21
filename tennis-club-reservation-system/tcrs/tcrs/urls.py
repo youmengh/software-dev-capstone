8d751125fbe95fa6e213cb30337c5ab4d46c0840
iancatania <78445705+iancatania@users.noreply.github.com>
2023-02-21 14:38:55 -0500

    Added basic home page view

--- tennis-club-reservation-system/tcrs/tcrs/urls.py
+++ tennis-club-reservation-system/tcrs/tcrs/urls.py
@@ -14,30 +14,9 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include  # new
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-]
-
-
-# TESTING HOME PAGE
-
-# Use include() to add paths from the main application
-from django.urls import include
-
-urlpatterns += [
-    path('home/', include('app.urls')),
-]
-
-#Add URL maps to redirect the base URL to our application
-from django.views.generic import RedirectView
-urlpatterns += [
-    path('', RedirectView.as_view(url='home/', permanent=True)),
-]
-
-# Use static() to add URL mapping to serve static files during development (only)
-from django.conf import settings
-from django.conf.urls.static import static
-
-urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    path("admin/", admin.site.urls),
+    path("", include("app.urls")),  # new
+]    
