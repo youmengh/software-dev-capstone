da183e2c067fd241adc70deffa075744825f7df4
iancatania <78445705+iancatania@users.noreply.github.com>
2023-04-03 13:35:57 -0400

    Added admin support for registration

--- tennis-club-reservation-system/tcrs/app/admin.py
+++ tennis-club-reservation-system/tcrs/app/admin.py
@@ -2,8 +2,9 @@ from django.contrib import admin
 
 # Register your models here.
 
-from .models import NewsFeed, MemberProfile, PaymentInfo
+from .models import NewsFeed, MemberProfile, PaymentInfo, Reservation
 
 admin.site.register(NewsFeed)
 admin.site.register(MemberProfile)
 admin.site.register(PaymentInfo)
+admin.site.register(Reservation)
